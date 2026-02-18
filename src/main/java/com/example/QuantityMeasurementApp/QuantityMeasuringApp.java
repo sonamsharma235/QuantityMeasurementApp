cff3514f321d90bed75206873f05c4d2c995a440
sonamsharma235 <trigunayatsonam@gmail.com>
2026-02-18 16:27:53 +0530

    [Sonam Sharma]Add. added UC1 of quntity measurement app

---
+++ src/main/java/com/example/QuantityMeasurementApp/QuantityMeasuringApp.java
@@ -0,0 +1,48 @@
+package com.example.QuantityMeasurementApp;
+
+public class QuantityMeasuringApp {
+	
+   public static class Feet{
+	   private final double value;
+	   
+	   public Feet(double value) {
+		   this.value=value;
+	   }
+	   
+	   @Override
+	   public boolean equals(Object obj) {
+		    
+		   if(this==obj) {
+			   return true;
+		   }
+		   
+		   if(obj==null) {
+			   return false;
+		   }
+		   
+		   if(getClass()!=obj.getClass()) {
+			   return false;
+		   }
+		   
+		   Feet f=(Feet)obj;
+		   
+		   return Double.compare(this.value, f.value)==0;
+	   }
+	   
+	   @Override
+	   public int hashCode() {
+		   return Double.hashCode(value);
+	   }
+   }
+   
+   public static void main(String[]args) {
+	   
+	   Feet v1=new Feet(1.0);
+	   Feet v2=new Feet(null);
+	   
+	   boolean result=v1.equals(v2);
+	   
+	   System.out.println("both are : "+result);
+	   
+   }
+}
---
+++ src/test/java/com/example/QuantityMeasurementApp/QuantityMeasurementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuantityMeasurementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityMeasurementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
