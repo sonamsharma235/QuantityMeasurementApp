99712bfee7884761b1915cf100917bdff21d0e10
sonamsharma235 <trigunayatsonam@gmail.com>
2026-03-17 15:04:22 +0530

    [Sonam Sharma]Refactor. refactor UC8

--- src/main/java/com/example/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/example/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -1,4 +1,4 @@
-package QuantityMeasurementApplication;
+package com.example.QuantityMeasurementApp;
 import java.util.*;
 public class QuantityMeasurementApp {
 	//generic method to demonstrate Length equality check
