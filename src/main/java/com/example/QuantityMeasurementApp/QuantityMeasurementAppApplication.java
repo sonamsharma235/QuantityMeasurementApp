3999599e1a4da30ff85400bbac81c11471022502
sonamsharma235 <trigunayatsonam@gmail.com>
2026-04-27 16:03:45 +0530

    [Sonam Sharma] Integrated backend and frontend:

--- src/main/java/com/example/QuantityMeasurementApp/QuantityMeasurementAppApplication.java
+++ src/main/java/com/example/QuantityMeasurementApp/QuantityMeasurementAppApplication.java
@@ -3,10 +3,18 @@ package com.example.QuantityMeasurementApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class QuantityMeasurementAppApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		
 		SpringApplication.run(QuantityMeasurementAppApplication.class, args);
 		System.out.println("Application started");
 	}
